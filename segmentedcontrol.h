29a90ea399490c1df551f7e705cd69bdcbb765e0
Flavio Tordini <flavio.tordini@gmail.com>
2023-07-11 13:22:51 +0200

    SegmentedControl

---
+++ segmentedcontrol.h
@@ -0,0 +1,45 @@
+#ifndef SEGMENTEDCONTROL_H
+#define SEGMENTEDCONTROL_H
+
+#include <QtWidgets>
+
+class SegmentedControl : public QWidget {
+    Q_OBJECT
+
+public:
+    SegmentedControl(QWidget *parent = nullptr);
+    QAction *addAction(QAction *action);
+    bool setCheckedAction(int index);
+    bool setCheckedAction(QAction *action);
+    QSize minimumSizeHint(void) const;
+
+signals:
+    void checkedActionChanged(QAction &action);
+
+protected:
+    void paintEvent(QPaintEvent *event);
+    void mouseMoveEvent(QMouseEvent *event);
+    void mousePressEvent(QMouseEvent *event);
+    void mouseReleaseEvent(QMouseEvent *event);
+    void leaveEvent(QEvent *event);
+
+private slots:
+    void setupColors();
+
+private:
+    void paintButton(QPainter *painter, const QRect &rect, const QAction *action);
+    QAction *findHoveredAction(const QPoint &pos) const;
+    int calculateButtonWidth() const;
+
+    QVector<QAction *> actionList;
+    QAction *checkedAction;
+    QAction *hoveredAction;
+    QAction *pressedAction;
+
+    QColor backgroundColor;
+    QColor selectedColor;
+    QColor hoveredColor;
+    QColor pressedColor;
+};
+
+#endif /* !SEGMENTEDCONTROL_H */
