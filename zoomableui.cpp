bdb79b7fe649363edcd59db06f6854a1c5e54840
Flavio Tordini <flavio.tordini@gmail.com>
2023-08-22 13:01:42 +0200

    new impl

--- zoomableui.cpp
+++ zoomableui.cpp
@@ -1,14 +1,17 @@
 #include "zoomableui.h"
 
 ZoomableUI::ZoomableUI(QMainWindow &window) : QObject(&window) {
-    // May be useful for font sizes set in stylesheets
-    // QCoreApplication::setAttribute(Qt::AA_UseStyleSheetPropagationInWidgetStyles, true);
+    // Enable font propagation for widgets that have stylesheets
+    QCoreApplication::setAttribute(Qt::AA_UseStyleSheetPropagationInWidgetStyles);
+
+    static int defaultZoom = 0;
+#ifdef Q_OS_MAC
+    defaultZoom = 2;
+#endif
 
     auto applyZoom = [](int zoom, bool saveSetting = true) {
-        // https://doc.qt.io/qt-6/qwidget.html#font-prop
-
         QSettings settings;
-        const int currentZoom = settings.value("zoom").toInt();
+        const int currentZoom = settings.value("zoom", defaultZoom).toInt();
 
         // reset
         int increment = zoom;
@@ -16,21 +19,37 @@ ZoomableUI::ZoomableUI(QMainWindow &window) : QObject(&window) {
             increment = -currentZoom;
         }
 
-        auto font = QApplication::font();
-        font.setPointSize(font.pointSize() + increment);
-        QApplication::setFont(font);
+#ifdef Q_OS_MAC
+        static auto defaultAppFont = QApplication::font();
+#endif
 
+        auto appFont = QApplication::font();
+        appFont.setPointSize(appFont.pointSize() + increment);
+        QApplication::setFont(appFont);
+
+        // fix font for widgets that have a custom font
+        // either by CSS or by QWidget::setFont
         for (auto w : QApplication::allWidgets()) {
-            auto font = w->font();
-            font.setPointSize(font.pointSize() + increment);
-            w->setFont(font);
+            if (w->testAttribute(Qt::WA_SetFont)) {
+                // has custom font
+                auto font = w->font();
+                font.setPointSize(font.pointSize() + increment);
+                w->setFont(font);
+            }
         }
+
+#ifdef Q_OS_MAC
+        // Mac style does not support bigger sizes for buttons
+        // https://bugreports.qt.io/browse/QTBUG-116337
+        if (QApplication::style()->name() != "Fusion")
+            QApplication::setFont(defaultAppFont, "QAbstractButton");
+#endif
 
         if (saveSetting) settings.setValue("zoom", currentZoom + increment);
     };
 
     QSettings settings;
-    int zoom = settings.value("zoom").toInt();
+    int zoom = settings.value("zoom", defaultZoom).toInt();
     if (zoom) applyZoom(zoom, false);
 
     auto createAction = [this, applyZoom, &window](QString text, QKeySequence keySeq, int zoom) {
@@ -38,8 +57,7 @@ ZoomableUI::ZoomableUI(QMainWindow &window) : QObject(&window) {
         action->setShortcut(keySeq);
         window.addAction(action);
         actions.append(action);
-        connect(action, &QAction::triggered, &window,
-                [&window, applyZoom, zoom] { applyZoom(zoom); });
+        connect(action, &QAction::triggered, this, [applyZoom, zoom] { applyZoom(zoom); });
     };
 
     createAction(tr("Zoom In"), QKeySequence(Qt::CTRL | Qt::Key_Plus), 1);
