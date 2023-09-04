cce50cd07424540244ea460fc2faff4ea8b65c81
Flavio Tordini <flavio.tordini@gmail.com>
2023-09-04 02:00:55 +0200

    remove spacer minwidth

--- spacer.cpp
+++ spacer.cpp
@@ -1,11 +1,7 @@
 #include "spacer.h"
 
-Spacer::Spacer(QWidget *parent, int minWidth) : QWidget(parent), minWidth(minWidth) {
+Spacer::Spacer(QWidget *parent) : QWidget(parent) {
     setAttribute(Qt::WA_TransparentForMouseEvents);
     setSizePolicy(QSizePolicy::MinimumExpanding, QSizePolicy::MinimumExpanding);
     setEnabled(false);
-}
-
-QSize Spacer::sizeHint() const {
-    return QSize(minWidth, 1);
 }
--- spacer.h
+++ spacer.h
@@ -1,17 +1,11 @@
 #ifndef SPACER_H
 #define SPACER_H
 
-#include <QtWidgets>
+#include <QtWidgets/QWidget>
 
 class Spacer : public QWidget {
 public:
-    Spacer(QWidget *parent = nullptr, int minWidth = 100);
-
-protected:
-    QSize sizeHint() const;
-
-private:
-    int minWidth;
+    Spacer(QWidget *parent = nullptr);
 };
 
 #endif // SPACER_H
