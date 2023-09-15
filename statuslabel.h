10e4f2d64ae036cbe4dc66f992e177bf0057e1cd
Flavio Tordini <flavio.tordini@gmail.com>
2023-09-15 15:20:21 +0200

    fix build

--- statuslabel.h
+++ statuslabel.h
@@ -1,7 +1,9 @@
 #ifndef STATUSLABEL_H
 #define STATUSLABEL_H
 
+#include <QtCore/QTimer>
 #include <QtWidgets/QLabel>
+#include <QtWidgets/QMainWindow>
 
 class StatusLabel : public QLabel {
     Q_OBJECT
